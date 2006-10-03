7f954bf0b66521476d5358713452750fcf832a95
hmelendez <hmelendez@570d4ea0-271c-0410-b6a8-5bf0f6f69dcc>
2006-10-03 02:21:14 +0000

    Etapa de rediseno, NO COMPILA. Falta reimplementar la interaccion del Search con el stack de estados

--- trunk/tesis-javapathfinder/src/tesis/extensiones/Mediator.java
+++ trunk/tesis-javapathfinder/src/tesis/extensiones/Mediator.java
@@ -1,17 +1,6 @@
 package tesis.extensiones;
 
 /**
- * See page 276 of DESIGN PATTERNS [1995].
- * Implemented by Blueprint Technologies, Inc.
- */
-
-/**
- * Package
- */
-
-//package com.blueprint.patterns.gamma.behavioral.mediator;
-
-/**
  * Defines an interface for communicating with Colleague objects.
  */
 
