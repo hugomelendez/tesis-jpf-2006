888572f697b07ba014f8195e2e56b034f936cdcb
rsomosa <rsomosa@570d4ea0-271c-0410-b6a8-5bf0f6f69dcc>
2006-12-11 23:25:39 +0000

    Corrección del bug que nos filtraba algunos OID en el executeInstruction

--- trunk/tesis-javapathfinder/src/tesis/extensiones/XMLReader.java
+++ trunk/tesis-javapathfinder/src/tesis/extensiones/XMLReader.java
@@ -12,11 +12,12 @@ public abstract class XMLReader {
 	 * @param file
 	 */
 	public XMLReader(String file) {
-		try {
+		//RS 20061211: Para que no me de error
+		/*try {
 			openFile(file);
 		} catch (DocumentException e) {
 			e.printStackTrace();
-		}
+		}*/
 	}
 
 	/**
