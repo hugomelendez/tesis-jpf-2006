cd0d35d70374471efd3849bc5d3bf40eb2426a90
mkiszkur <mkiszkur@570d4ea0-271c-0410-b6a8-5bf0f6f69dcc>
2006-12-14 23:08:58 +0000

    Es un ejemplo usado para el documento

---
+++ trunk/tesis-javapathfinder/src/tesis/Doc_Ejemplo_01/ModeloVacio.java
@@ -0,0 +1,18 @@
+package tesis.Doc_Ejemplo_01;
+
+public class ModeloVacio {
+
+	private static int MetodoDePrueba(){
+		return 0;
+		
+	}
+	public static void main (String[] args) {
+		int a=100;
+		a = MetodoDePrueba();
+	}	
+}
+
+
+
+
+
