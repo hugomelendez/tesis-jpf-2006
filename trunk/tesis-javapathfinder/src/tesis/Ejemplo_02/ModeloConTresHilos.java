ff5861c43c116a276dfc9319227f76161f746064
rsomosa <rsomosa@570d4ea0-271c-0410-b6a8-5bf0f6f69dcc>
2007-06-19 20:48:47 +0000

    Adaptación de Ej02. Corrección de condición de backtrackeo en DFSearchTesis

--- trunk/tesis-javapathfinder/src/tesis/Ejemplo_02/ModeloConTresHilos.java
+++ trunk/tesis-javapathfinder/src/tesis/Ejemplo_02/ModeloConTresHilos.java
@@ -20,15 +20,81 @@ class Hilo implements Runnable {
 	}
 }
 
+class HiloA implements Runnable {
+	/**
+	 * El nombre del Evento
+	 */
+	String nombreEvento;
+
+	/**
+	 * Referencia al otro Thread
+	 */
+	Hilo other;
+
+	public HiloA(String name) {
+		this.nombreEvento = name;
+	}
+
+	public void run() {
+		imprimirA();
+	}
+	
+	private void imprimirA() {
+		System.out.println("EVENTO " + nombreEvento + this.getClass().getName());		
+	}
+}
+
+class HiloB implements Runnable {
+	/**
+	 * El nombre del Evento
+	 */
+	String nombreEvento;
+
+	/**
+	 * Referencia al otro Thread
+	 */
+	Hilo other;
+
+	public HiloB(String name) {
+		this.nombreEvento = name;
+	}
+
+	public void run() {
+		imprimirB();
+	}
+	
+	private void imprimirB() {
+		System.out.println("EVENTO " + nombreEvento + this.getClass().getName());		
+	}
+}
+
+class HiloC implements Runnable {
+	/**
+	 * El nombre del Evento
+	 */
+	String nombreEvento;
+
+	public HiloC(String name) {
+		this.nombreEvento = name;
+	}
+
+	public void run() {
+		imprimirC();
+	}
+	
+	private void imprimirC() {
+		System.out.println("EVENTO " + nombreEvento + this.getClass().getName());		
+	}
+}
+
 public class ModeloConTresHilos {
 
 	public static void main(String[] args) {
-		Hilo o1 = new Hilo("A");
-		Hilo o2 = new Hilo("B");
-		Hilo o3 = new Hilo("C");
-
-		o1.other = o2;
-		o2.other = o1;
+		String s = new String("Hilo MAGIA");
+		
+		HiloA o1 = new HiloA(s);
+		HiloB o2 = new HiloB(s);
+		HiloC o3 = new HiloC(s);
 
 		Thread t1 = new Thread(o1);
 		Thread t2 = new Thread(o2);
