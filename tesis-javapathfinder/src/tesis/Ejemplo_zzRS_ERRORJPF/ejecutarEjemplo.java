7a6a2798b7c884854cdf623945c49aca5b7fb33c
rsomosa <rsomosa@570d4ea0-271c-0410-b6a8-5bf0f6f69dcc>
2007-06-22 00:27:01 +0000

    Agregado VerificationLauncher para encapsular la ejecución de la verificación

--- tesis-javapathfinder/src/tesis/Ejemplo_zzRS_ERRORJPF/ejecutarEjemplo.java
+++ tesis-javapathfinder/src/tesis/Ejemplo_zzRS_ERRORJPF/ejecutarEjemplo.java
@@ -1,35 +1,9 @@
 package tesis.Ejemplo_zzRS_ERRORJPF;
 
-import gov.nasa.jpf.Config;
-import gov.nasa.jpf.JPF;
 import tesis.extensiones.*;
 
 public class ejecutarEjemplo {
 	public static void main (String[] args) throws XMLException {
-		// Path para acceder a este ejemplo, es el prefijo usado para leer los xmls
-		String path = "./src/tesis/Ejemplo_zzRS_ERRORJPF/";
-
-		 Coordinador c = new Coordinador();
-		 c.loadConfiguration(path + "Events.xml", path + "ProblemProperty.xml", path + "ProblemContext.xml");
-//		 c.loadConfiguration(path + "Events.xml", path + "ProblemProperty.xml");
-
-		Listener listener = new Listener(c);
-		
-		String[] a = new String[1];
-		a[0] = "tesis.Ejemplo_zzRS_ERRORJPF.Modelo";
-	    Config conf = JPF.createConfig(a);
-
-	    // usamos nuestra busqueda
-	    conf.setProperty("search.class","tesis.extensiones.DFSearchTesis");
-
-	    JPF jpf = new JPF(conf);
-	    jpf.addSearchListener(listener);
-	    jpf.addVMListener(listener);
-	    
-	    ((DFSearchTesis)jpf.search).setCoordinador(c);
-	    
-	    System.out.println("---------------- JPF started");
-	    jpf.run();
-	    System.out.println("---------------- JPF terminated");
+		VerificationLauncher.execute("./src/tesis/Ejemplo_zzRS_ERRORJPF/", "tesis.Ejemplo_zzRS_ERRORJPF.Modelo");
 	}
 }
