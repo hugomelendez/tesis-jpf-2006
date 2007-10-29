a08ab24829d823a539aae1986812cc66ca686f3a
hmelendez <hmelendez@570d4ea0-271c-0410-b6a8-5bf0f6f69dcc>
2007-10-29 16:40:44 +0000

    nuevo metodo static para ejecutar un ejemplo sin agregado del framework

--- trunk/tesis-javapathfinder/src/tesis/extensiones/VerificationLauncher.java
+++ trunk/tesis-javapathfinder/src/tesis/extensiones/VerificationLauncher.java
@@ -1,5 +1,8 @@
 package tesis.extensiones;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 import gov.nasa.jpf.Config;
 import gov.nasa.jpf.JPF;
 
@@ -42,5 +45,45 @@ public class VerificationLauncher {
 	    System.out.println("---------------- JPF started");
 	    jpf.run();
 	    System.out.println("---------------- JPF terminated");
+	}
+
+	/**
+	 * Ejecucion sin framework, solo JPF original
+	 * @param modelo: clase principal de la aplicación a verificar
+	 * @throws XMLException
+	 */
+	public static void execute(String modelo) {
+//		Coordinador c = new Coordinador();
+//
+//		if (usarContexto) {
+//			c.loadConfiguration(pathXML + "Events.xml", pathXML + "ProblemProperty.xml", pathXML + "ProblemContext.xml");
+//		} else {
+//			c.loadConfiguration(pathXML + "Events.xml", pathXML + "ProblemProperty.xml");
+//		}
+//
+//		Listener listener = new Listener(c);
+		
+		String[] a = new String[1];
+		a[0] = new String(modelo);
+	    Config conf = JPF.createConfig(a);
+
+	    // Búsqueda custom del FWK
+//	    conf.setProperty("search.class","tesis.extensiones.DFSearchTesis");
+
+	    JPF jpf = new JPF(conf);
+//	    jpf.addSearchListener(listener);
+//	    jpf.addVMListener(listener);
+//	    
+//	    ((DFSearchTesis)jpf.search).setCoordinador(c);
+	    
+		System.out.println("---------------- JPF started "+now());
+	    jpf.run();
+	    System.out.println("---------------- JPF terminated "+now());
+	}
+
+	private static String now() {
+		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		java.util.Date date = new java.util.Date();
+		return (dateFormat.format(date));
 	}
 }
